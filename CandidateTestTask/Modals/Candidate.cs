b9ecc2b6d3bb3986940f1847a6b6e23dff79d7bb
prabin <p.stha@hotmail.com>
2024-11-13 22:44:38 +0545

    Implement Candidate model and CandidateDto for data structure

---
+++ CandidateTestTask/Modals/Candidate.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CandidateTestTask.Modals
+{
+    public class Candidate
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [MaxLength(15)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } // Unique Identifier
+
+        public string PreferredCallTime { get; set; }
+
+        [Url]
+        public string LinkedInProfile { get; set; }
+
+        [Url]
+        public string GitHubProfile { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Comments { get; set; }
+    }
+}
