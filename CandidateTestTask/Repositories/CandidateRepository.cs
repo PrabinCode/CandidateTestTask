4d6df19878b27e61a7370d913177809f14452229
prabin <p.stha@hotmail.com>
2024-11-13 23:06:45 +0545

    Add ICandidateRepository and CandidateRepository for data access

---
+++ CandidateTestTask/Repositories/CandidateRepository.cs
@@ -0,0 +1,41 @@
+﻿using CandidateTestTask.Data;
+using CandidateTestTask.Modals;
+
+namespace CandidateTestTask.Repositories
+{
+    public class CandidateRepository : ICandidateRepository
+    {
+        private readonly CandidateDbContext _context;
+
+        public CandidateRepository(CandidateDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Candidate> GetCandidateByEmailAsync(string email)
+        {
+            return await _context.Candidates.FirstOrDefaultAsync(c => c.Email == email);
+        }
+
+        public async Task AddOrUpdateCandidateAsync(Candidate candidate)
+        {
+            var existingCandidate = await GetCandidateByEmailAsync(candidate.Email);
+            if (existingCandidate == null)
+            {
+                await _context.Candidates.AddAsync(candidate);
+            }
+            else
+            {
+                existingCandidate.FirstName = candidate.FirstName;
+                existingCandidate.LastName = candidate.LastName;
+                existingCandidate.PhoneNumber = candidate.PhoneNumber;
+                existingCandidate.PreferredCallTime = candidate.PreferredCallTime;
+                existingCandidate.LinkedInProfile = candidate.LinkedInProfile;
+                existingCandidate.GitHubProfile = candidate.GitHubProfile;
+                existingCandidate.Comments = candidate.Comments;
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ CandidateTestTask/Repositories/ICandidateRepository.cs
@@ -0,0 +1,10 @@
+﻿using CandidateTestTask.Modals;
+
+namespace CandidateTestTask.Repositories
+{
+    public interface ICandidateRepository
+    {
+        Task<Candidate> GetCandidateByEmailAsync(string email);
+        Task AddOrUpdateCandidateAsync(Candidate candidate);
+    }
+}
