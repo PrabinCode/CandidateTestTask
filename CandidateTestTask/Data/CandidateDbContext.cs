6951db674b568aed694f0486ee3df5fbf71aa431
prabin <p.stha@hotmail.com>
2024-11-13 22:55:04 +0545

    Create CandidateDbContext and initial migrations

---
+++ CandidateTestTask/Data/CandidateDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CandidateTestTask.Modals;
+using Microsoft.EntityFrameworkCore;
+
+namespace CandidateTestTask.Data
+{
+    public class CandidateDbContext : DbContext
+    {
+        public CandidateDbContext(DbContextOptions<CandidateDbContext> options) : base(options) { }
+
+        public DbSet<Candidate> Candidates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Candidate>()
+                .HasIndex(c => c.Email)
+                .IsUnique(); // Ensuring Email as unique identifier
+        }
+    }
+}
